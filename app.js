1406ace37cc4fc71bbf9223422d3df1bcbd3e49a
Dennis Kestone <denniskestone@gmail.com>
2023-08-14 16:10:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.set('view engine', 'ejs');
 // Middleware route
 app.use('/', weatherRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server starting at port ${PORT}`));
